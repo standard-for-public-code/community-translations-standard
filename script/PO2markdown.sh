ec10828c543b350d056eaf5e86108b041c4a95a7
Silvério Santos <ssantos@web.de>
2022-06-18 21:31:14 +0200

    Added header with usage, corrected folder creation.
    Both scripts echo how to use when run.
    The pot folder was created too late, moved up.

--- script/PO2markdown.sh
+++ script/PO2markdown.sh
@@ -15,11 +15,28 @@ declare -a filearray=('index' 'introduction' 'readers-guide' 'glossary' 'CONTRIB
 
 #####
 
+# Echoes a header to standard output
+function echoHeader() {
+    echo "Syntax:"
+    echo " ./PO2markdown.sh"
+    echo "Purpose:"
+    echo " Coverts  from (potentially translated) language specific .po"
+    echo " files and the original .md files into translated .md files (if done)"
+    echo " in a folder named after the lang code, on the pot folder level."
+    echo "How to:"
+    echo " cd into the scripts subfolder and run this scrpt without parameters."
+    echo "Configure  languages:"
+    echo " the same langs array as in markdown2PO.sh"
+    echo 
+}
+
 function poToMdFile() {
     for lang in ${langs[@]};  do
-        po2md -q --po-encoding UTF-8 -p ../pot/$lang/$1.po -s../$lang/$1.md ../../standard/$1.md
+        po2md -q -w 0  --po-encoding UTF-8 -p ../pot/$lang/$1.po -s../$lang/$1.md ../../standard/$1.md
     done
 }
+
+echoHeader
 
 # Create folders for translated MD files, if not exist
 for lang in ${langs[@]};  do
@@ -30,11 +47,13 @@ done
 
 # Translates single PO files into MD files
 for filebody in ${filearray[@]}; do
+    echo $filebody
     poToMdFile $filebody
 done
 
 # Translates PO files in subfolder criteria into MD files
 for path in $(find ../pot/criteria -type f -name '*.pot'); do
+    echo "criteria/"`basename ${path}`
     poToMdFile "/criteria/"`basename ${path} .pot`
 done
 
--- script/markdown2PO.sh
+++ script/markdown2PO.sh
@@ -14,10 +14,29 @@ declare -a filearray=('index' 'introduction' 'readers-guide' 'glossary' 'CONTRIB
 
 #####
 
+# Echoes a header to standard output
+function echoHeader() {
+    echo "Syntax:"
+    echo " ./markdown2PO.sh"
+    echo "Purpose:"
+    echo " Converts .md markdown files into .pot files with folder structure"
+    echo " and then creates/updates (non-destructive) .po files in the configured"
+    echo " language's subfolders."
+    echo "How to:"
+    echo " git clone standard and community-translations-standard"
+    echo " from the online project (currently: https://github.com/publiccodenet/),"
+    echo " cd into the latter's scripts subfolder and run this script. Then start"
+    echo " translating the .po files in the pot/<language code> folder."
+    echo "Configure languages:"
+    echo " Add '<language code>' separated by a blank space to the langs array"
+    echo " in the sript source code."
+    echo 
+}
+
 # Creates a POT file and merges it with respective language specific PO file
 # Parameters: File basename of the MD file (no extension), optionally prepended by the relative path
 function mdToPoFile() {
-    md2po -q --po-encoding UTF-8 -s -po ../pot/$1.pot ../../standard/$1.md
+    md2po -q -d 'Content-Type: text/plain; charset=UTF-8' -d 'Language: en' --po-encoding UTF-8 -s -po ../pot/$1.pot ../../standard/$1.md
     for lang in ${langs[@]};  do
         if [ -f ../pot/$lang/$1.po ]; then
             msgmerge -U ../pot/$lang/$1.po ../pot/$1.pot;
@@ -27,16 +46,19 @@ function mdToPoFile() {
     done
 }
 
+echoHeader
+
+# Create POT criteria subfolder, if not exists
+if [ ! -d ../pot/criteria ]; then
+    mkdir -p ../pot/criteria
+fi
+
 # Translates single files from MD to POT and language specific POs
 for filebody in ${filearray[@]}; do
     mdToPoFile $filebody
 done
 
 # subfolder: criteria
-# Create POT criteria subfolder, if not exists
-if [ ! -d ../pot/criteria ]; then
-    mkdir ../pot/criteria
-fi
 # Create translation PO subfolders, if not exist
 for lang in ${langs[@]};  do
     if [ ! -d ../pot/$lang/criteria ]; then
